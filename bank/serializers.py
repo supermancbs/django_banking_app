41b2daa027c83ebf7f5544b8a9eae93f8f49ff26
supermancbs <clark.sacktor@gmail.com>
2016-09-15 18:08:42 -0400

    added serializer

--- bank/serializers.py
+++ bank/serializers.py
@@ -1,5 +1,20 @@
-from django.contrib.auth.models import User, Withdrawl
+from django.contrib.auth.models import User, Withdrawl, Deposit, Transaction
 from rest_framework import serializers
+
+class WithdrawlSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Withdrawl
+        fields = ('amount', 'created_at')
+
+class DepositSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Deposit
+        fields = ('amount', 'created_at')
+
+class TransactionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Transaction
+        fields = ('amount', 'created_at')
 
 class UserSerializer(serializers.ModelSerializer):
     tracks = serializers.StringRelatedField(many=True)
